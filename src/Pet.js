9be87fc27be188d9177159c4963c6ff0984a22a1
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2023-05-07 18:21:24 -0400

    pet component is added

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,26 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Pet = (props) => ( 
-    <p> {props.name} | {props.breed} | {props.animal} </p> 
- )
+const Pet = (props) => {
+  
+  const {name, animal , breed, location , id , images} = props;
+  let petImage = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if(images.length){
+    petImage = images[0]
+  }
+
+  return (
+    <Link  to={`/details/${id}`} className="pet">
+    <div className="image-container">
+    <img src={petImage} alt={name} />
+    </div>
+    <div className="info">
+     <h1>{name}</h1> 
+     <h2>{`${animal} — ${breed} — ${location}`}</h2>
+    </div>
+    </Link>
+  )
+  
+}
 
 export default Pet;
-
-
